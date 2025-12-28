6326fa469d927e85b30105c342546afad1c47173
Judy Hamoudeh <judyhammoudeh@gmail.com>
2025-12-29 00:27:31 +0300

    satisfying requirement change two

--- hmk1/RangePrinter.java
+++ hmk1/RangePrinter.java
@@ -7,8 +7,11 @@ public class RangePrinter {
         int b = sc.nextInt();
 
         for (int i = a + 1; i < b; i++) {
-            System.out.println(i);
-        }
+    if (i % 2 != 0) {
+        System.out.println(i);
+    }
+}
+
     }
 }
 

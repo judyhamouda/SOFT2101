daa6119fc9dc2f2b58a48611d0389a32e5ddf16c
Judy Hamoudeh <judyhammoudeh@gmail.com>
2025-12-30 18:47:23 +0300

    Fix missing brace in RangePrinter

--- hmk1/RangePrinter.java
+++ hmk1/RangePrinter.java
@@ -1,37 +1,20 @@
 import java.util.Scanner;
 
 public class RangePrinter {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
         for (int i = a + 1; i < b; i++) {
             if (i % 2 == 0) {
-
-        System.out.print("Enter first number: ");
-        int a = input.nextInt();
-
-        System.out.print("Enter second number: ");
-        int b = input.nextInt();
-
-        if (a < b) {
-            for (int i = a + 1; i < b; i++) {
-                System.out.println(i);
-            }
-        } else {
-            for (int i = b + 1; i < a; i++) {
-
                 System.out.println(i);
             }
         }
-
-        input.close();
-    if (i % 2 != 0) {
-        System.out.println(i);
     }
 }
 
-    }
-}
 
 
